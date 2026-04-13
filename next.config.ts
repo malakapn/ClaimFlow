6a218c6720cd63587451c95b518a1794345a3310
Paramita Malakar <paramitam@gmail.com>
2026-04-12 18:11:18 -0700

    Add standalone output option to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+ts
+  import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
 };
 
 export default nextConfig;
